4b85af51cd8961e9fb287079aa62e1ff45c4012f
mbenniston <matthewpbenniston@gmail.com>
2023-06-15 10:02:30 +0100

    Add margin for project cards when on mobile

--- app/project_card.tsx
+++ app/project_card.tsx
@@ -13,8 +13,8 @@ export type ProjectData = {
 export function ProjectCard(props: { data: ProjectData }) {
   return (
     <Card className="text-black">
-      <div className="flex justify-center items-center">
-        <div className="relative w-64 h-64 lg:h-full">
+      <div className="flex justify-center items-center mt-8 lg:mt-0">
+        <div className="relative w-64 h-64 lg:h-full ">
           <Link href={props.data.link}>
             <figure className=""> <Image src={props.data.imageURL} alt={props.data.title} fill className="block shadow object-cover object-center lg:rounded-l-lg" /> </figure>
           </Link>
